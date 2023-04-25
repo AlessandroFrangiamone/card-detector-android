196792436ed6c54a0607cbf258356d2a9c6ad951
Alessandro <alessandro.frangiamone96@gmail.com>
2023-04-26 00:25:09 +0200

    Init project

---
+++ app/src/main/java/it/alessandrof/carddetector/CardDetectorApplication.kt
@@ -0,0 +1,15 @@
+package it.alessandrof.carddetector
+
+import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
+
+class CardDetectorApplication: Application() {
+
+    @Override
+    override fun onCreate() {
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        super.onCreate()
+    }
+
+}
